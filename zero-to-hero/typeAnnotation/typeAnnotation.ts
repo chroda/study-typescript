7730e8489067b2d40dbd4cc54782f4748c8e4b30
Christian Oliveira <coliveira@quanticosolutions.com>
2023-07-10 17:13:35 +0100

    update

--- zero-to-hero/typeAnnotation/typeAnnotation.ts
+++ zero-to-hero/typeAnnotation/typeAnnotation.ts
@@ -18,7 +18,7 @@ console.log(car);
 function multiply(a: number, b: number): number {
   return a * b;
 }
-console.log(multiply(2, 5));
+console.log(multiply(2, 3));
 
 // ==> Functions with void
 function print(value: any): void {
@@ -31,3 +31,15 @@ function throwError(errorMsg: string): never {
   throw new Error(errorMsg);
 }
 // throwError("Error");
+
+let num1: number = 23.0;
+let num2: number = 0x78cf;
+let num3: number = 0o577;
+let num4: number = 0b110001;
+
+console.log(num1);
+console.log(num2);
+console.log(num3);
+console.log(num4);
+
+// let bitint: bigint = 100n;
